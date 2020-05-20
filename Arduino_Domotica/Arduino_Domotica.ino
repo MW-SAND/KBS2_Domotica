f1aed5ff843ca075a1d17f96af2c4184e2d87b97
Mattias <s1142173@student.windesheim.nl>
2020-05-20 14:26:42 +0200

    Arduino + RPi code

--- Arduino_Domotica/Arduino_Domotica.ino
+++ Arduino_Domotica/Arduino_Domotica.ino
@@ -1,28 +1,53 @@
 #define lightSensor A0
+int lighting = 11;
+int heater = 12;
+boolean lightingState = false;
+boolean heaterState = false;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(lightSensor, INPUT);
+  pinMode(lighting, OUTPUT);
+  pinMode(heater, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
     char incomingByte = (char)Serial.read();
     if (incomingByte == 'g') {
-      delay(100);
-      char secondByte = (char)Serial.read();
-      if (secondByte == 'b') {
         sendBrightness();
-      }
+    } else if (incomingByte == 'l') {
+        setLighting();
+    } else if (incomingByte == 'h') {
+        setHeater();
     }
   }
+  delay(30);
 }
 
 void sendBrightness() {
   int brightness = analogRead(lightSensor);
   
-  Serial.write(brightness);
+  Serial.print(brightness);
   Serial.flush();
+}
+
+void setLighting() {
+  if (lightingState) {
+    digitalWrite(lighting, LOW);
+  } else {
+    digitalWrite(lighting, HIGH);    
+  }
+
+  lightingState = !lightingState;
+}
+
+void setHeater()  {
+  if (heaterState)  {
+    digitalWrite(heater, LOW);
+  } else {
+    digitalWrite(heater, HIGH);
+  }
+
+  heaterState = !heaterState;
 }
